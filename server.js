ba23a3470bd5c2eff7bb9805e72a2c62bb4259b6
nayem-ahmedz <nayemahmed2n@gmail.com>
2025-11-13 04:21:25 +0600

    another try to deploy on vercel

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // db connection
     const db = client.db('bite_share');
