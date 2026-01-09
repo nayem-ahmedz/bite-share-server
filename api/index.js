dafa48a8b477a41b19e76f0ca21fe5d0b5b99c2d
nayem-ahmedz <nayemahmed2n@gmail.com>
2026-01-10 00:48:03 +0600

    vercel serverless api test

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
-// api/index.js
-const app = require("../app");
-const connectDB = require("../config/connectDB");
+// // api/index.js
+// const app = require("../app");
+// const connectDB = require("../config/connectDB");
 
-// Serverless function wrapper
-module.exports = async (req, res) => {
-  try {
-    await connectDB(); // safe to call on every request
-    return app(req, res); // delegate to Express app
-  } catch (err) {
-    console.error("DB connection error:", err.message);
-    res.status(500).json({ status: false, message: "Database connection failed" });
-  }
-};
+// // Serverless function wrapper
+// module.exports = async (req, res) => {
+//   try {
+//     await connectDB(); // safe to call on every request
+//     return app(req, res); // delegate to Express app
+//   } catch (err) {
+//     console.error("DB connection error:", err.message);
+//     res.status(500).json({ status: false, message: "Database connection failed" });
+//   }
+// };
+
+export default function handler(req, res){
+  res.send({message: 'welcome'});
+}
